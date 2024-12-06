11505ec5a7eaa4b1ec18c5dd4f11e8618d687a8c
Po-Cheng Yeh <blp100x@hotmail.com>
2024-12-06 18:32:26 +0800

    feat: add winner text for game end state

---
+++ src/app/components/WinnerText.tsx
@@ -0,0 +1,22 @@
+import { Center, Float, Text3D } from "@react-three/drei";
+
+type WinnerTextProps = {
+  color: string | number;
+  text: string;
+  [x: string]: any;
+};
+
+const WinnerText = (props: WinnerTextProps) => {
+  const { color, text, ...otherProps } = props;
+
+  return (
+    <Center top position={[0, 2.5, 0]} rotation={[0, 0, 0]} {...otherProps}>
+      <Text3D font="./fonts/Mansalva_Regular.json">
+        {text}
+        <meshBasicMaterial color={color} />
+      </Text3D>
+    </Center>
+  );
+};
+
+export default WinnerText;
