c5607f7b634b727df818ca4a95ad51c07c9929e8
Po-Cheng Yeh <blp100x@hotmail.com>
2024-12-12 18:41:23 +0800

    feat: add lights into scene

---
+++ src/app/components/canvas/Light.tsx
@@ -0,0 +1,83 @@
+// import { useHelper } from "@react-three/drei";
+// import { useRef } from "react";
+// import {
+//   DirectionalLight,
+//   DirectionalLightHelper,
+//   SpotLight,
+//   SpotLightHelper,
+//   OrthographicCamera,
+//   PerspectiveCamera,
+//   CameraHelper,
+// } from "three";
+
+export default function Light() {
+  /**
+   * Light Helper
+   */
+  // const dirLight = useRef<DirectionalLight>(null);
+  // const dirLightShadowHelper = useRef<OrthographicCamera>(null);
+  // const spotLight1 = useRef<SpotLight>(null);
+  // const spotLightShadowHelper1 = useRef<PerspectiveCamera>(null);
+  // const spotLight2 = useRef<SpotLight>(null);
+  // const spotLightShadowHelper2 = useRef<PerspectiveCamera>(null);
+  // useHelper(dirLight, DirectionalLightHelper, 1, "red");
+  // useHelper(dirLightShadowHelper, CameraHelper);
+  // useHelper(spotLight1, SpotLightHelper, 1);
+  // useHelper(spotLightShadowHelper1, CameraHelper);
+  // useHelper(spotLight2, SpotLightHelper, 1);
+  // useHelper(spotLightShadowHelper2, CameraHelper);
+
+  return (
+    <>
+      <ambientLight intensity={1.5} />
+      <spotLight
+        // ref={spotLight1}
+        castShadow
+        position={[4, 10, 8]}
+        angle={0.3}
+        penumbra={0.5}
+        intensity={2.1}
+        decay={0.3}
+      >
+        <perspectiveCamera
+          // ref={spotLightShadowHelper1}
+          attach="shadow-camera"
+          far={15}
+          near={5}
+        />
+      </spotLight>
+      <spotLight
+        // ref={spotLight2}
+        castShadow
+        position={[-4, 8, 8]}
+        angle={0.2}
+        penumbra={0.5}
+        intensity={0.9}
+        decay={0.2}
+      >
+        <perspectiveCamera
+          // ref={spotLightShadowHelper2}
+          attach="shadow-camera"
+          far={15}
+          near={5}
+        />
+      </spotLight>
+      <pointLight position={[-5, -5, -5]} />
+      <directionalLight
+        // ref={dirLight}
+        castShadow
+        position={[-4.6, 9, 9]}
+        intensity={0.9}
+        color={0xffffff}
+        shadow-mapSize={[1024, 1024]}
+        shadow-normalBias={0.05}
+      >
+        <orthographicCamera
+          // ref={dirLightShadowHelper}
+          attach="shadow-camera"
+          args={[-10, 10, 10, -10, 10, 20]}
+        />
+      </directionalLight>
+    </>
+  );
+}
